4b6b7b63355ac2e84f6f4607a03bdc9164ade959
Pavel Odstrčilík <pavel.odstrcilik@b2a.cz>
2022-02-14 13:31:07 +0100

    Init project

---
+++ Application/AttendanceApp/AttendanceApp/AttendanceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AttendanceAppApp.swift
+//  AttendanceApp
+//
+//  Created by Pavel Odstrčilík on 14.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct AttendanceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
